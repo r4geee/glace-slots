e1917ab720497ff0bf1663d001d018fe8a42f878
Alexei Rostovshchikov <alexei.rosto@gmail.com>
2018-01-11 00:43:05 +0200

    first tc for testdata

---
+++ tests/functional/main.js
@@ -0,0 +1,29 @@
+"use strict";
+var path = require("path");
+
+var gamePage = require(path.resolve(process.cwd(), 'lib', 'pages')).gamePage;
+
+
+test("Open page test", () => {
+
+    before(() => {
+        SS.registerPages(gamePage);
+    });
+
+    chunk("Open Test Page", async () => {
+        await SS.openTestPage();
+    });
+
+    chunk("Make a spin", async () => {
+        await SS.openTestPage();
+        await SS.spin();
+    });
+
+    chunk("Make a spin for testdata 111", async () => {
+        await SS.openTestPage();
+        await SS.setTestData('11122');
+        await SS.spin();
+
+        expect(await gamePage.winbox.getText()).equal('Win 60 coins');
+    });
+});
