0be5202a5e8fcac4f11305fc9063e0e26a807b3f
Alexei Rostovshchikov <alexei.rosto@gmail.com>
2018-01-13 22:31:08 +0200

    in work

--- tests/functional/testMain.js
+++ tests/functional/testMain.js
@@ -3,27 +3,24 @@ var path = require("path");
 
 var gamePage = require(path.resolve(process.cwd(), 'lib', 'pages')).gamePage;
 
-test("Some test", () => {
+test("Base", () => {
 
     before(() => {
         SS.registerPages(gamePage);
     });
 
-    // chunk("Open Test Page", async () => {
-    //     await SS.openTestPage();
-    // });
+    chunk("Open Test Page", async () => {
+        await SS.openTestPage();
+    });
 
-    // chunk("Make a spin", async () => {
-    //     await SS.openTestPage();
-    //     await SS.spin();
-    // });
+    chunk("Make a spin", async () => {
+        await SS.openTestPage();
+        await SS.spin();
+    });
 
     chunk("Make a spin for testdata 111", async () => {
-        await SS.openTestPage();
-        await SS.setTestData('11122');
-        await SS.spin();
-
-        expect(await gamePage.winbox.getText()).equal('Win 60 coins');
-        await SS.checkPaytableRowIsHighlighted('paytableRow111');
+        await SS.spinAndCheckWinAndPaytable('111');
     });
 });
+
+test("")
